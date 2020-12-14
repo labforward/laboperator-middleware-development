5d06bb49e729acc39b2516bdb7d0a7ab87fa2475
Ignatius Reza <lyoneil.de.sire@gmail.com>
2020-12-14 13:25:44 +0100

    [CUB-2816] Init should generate runnable middleware
    Ideally, we should have CI to ensure this, but too complicated
    for now

--- src/templates/init/src/routes/index.ts
+++ src/templates/init/src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Express, Request, Response } from 'express';
-import { fetch } from 'laboperator-middleware/helpers';
+import { fetch, jsonResponse } from 'laboperator-middleware/helpers';
 
-module.exports = (app: Express): void => {
+export default (app: Express): void => {
   app.get('/ping', async (_req: Request, res: Response) => {
     const response = await fetch({ url: 'http://example.com/ping' });
 
-    res.json(helpers.jsonResponse(200, response.body));
+    res.json(jsonResponse(200, response.body));
   });
 };
