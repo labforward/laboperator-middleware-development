cbeffbc69a0ce44d34a103f6b53c2196e6a604e8
Ignatius Reza <lyoneil.de.sire@gmail.com>
2021-11-18 11:20:41 +0100

    reconfigure linters

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,10 @@
-module.exports = require('labforward-config-base/prettier');
+let config;
+
+try {
+  config = require('@labforward/config/prettier');
+} catch {
+  // fallback for environments which does not support `exports` key in package.json yet, e.g. Atom editor
+  config = require('@labforward/config/.prettierrc');
+}
+
+module.exports = config;
